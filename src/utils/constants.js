73e352142a5551e1252829d9260d5851fd7caaf1
raiankit12 <ankit789101@gmail.com>
2024-10-07 00:05:22 +0530

    Remove API key

---
+++ src/utils/constants.js
@@ -0,0 +1,23 @@
+export const logo="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
+
+export const bgImg='https://assets.nflxext.com/ffe/siteui/vlv3/b2c3e95b-b7b5-4bb7-a883-f4bfc7472fb7/19fc1a4c-82db-4481-ad08-3a1dffbb8c39/IN-en-20240805-POP_SIGNUP_TWO_WEEKS-perspective_WEB_24a485f6-1820-42be-9b60-1b066f1eb869_medium.jpg';
+
+
+export const user_avatar= "https://occ-0-2483-3646.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABTZ2zlLdBVC05fsd2YQAR43J6vB1NAUBOOrxt7oaFATxMhtdzlNZ846H3D8TZzooe2-FT853YVYs8p001KVFYopWi4D4NXM.png?r=229";
+
+
+export const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+  Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMGM3MzczNmEyYjIwYmQyODBmMmVjZjEyOWE3NTkyOCIsIm5iZiI6MTcyNTM4NTY1MC4wMjg5NjgsInN1YiI6IjY2Y2I5MGNhODdkMGU3MTM0NTZlOWQyMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.d9ItmG6q8PW26eDYkpoQMMI30v4wc1U0TOw6UZ69mHY',
+  }
+};
+
+export const Suported_language=[
+  {identifier:"eng",name:"English"},
+  {identifier:"hindi",name:"Hindi"},
+  {identifier:"spanish",name:"Spanish"}]
+
+export const img_cdn="https://image.tmdb.org/t/p/w300";
+
