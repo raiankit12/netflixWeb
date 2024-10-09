bc54c973f83729db4626f226ef358e07d8a8a37a
raiankit12 <ankit789101@gmail.com>
2024-10-09 23:11:00 +0530

    changes in login page validation

--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -1,16 +1,22 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { createLogger } from "redux-logger"; // Import the logger
 import userReducer from "../utils/userSlice";
 import moviesReducer from "../utils/movieSlice";
-import gptReducer from '../utils/gptSlice';
-import configReducer from '../utils/configSlice'
-const appStore=configureStore({
-    reducer:{
-        user:userReducer,
-        movies:moviesReducer,
-        gpt:gptReducer,
-        config:configReducer,
-    },
+import gptReducer from "../utils/gptSlice";
+import configReducer from "../utils/configSlice";
 
+// Create the logger middleware
+const logger = createLogger();
+
+const appStore = configureStore({
+  reducer: {
+    user: userReducer,
+    movies: moviesReducer,
+    gpt: gptReducer,
+    config: configReducer,
+  },
+  middleware: (getDefaultMiddleware) => 
+    getDefaultMiddleware().concat(logger), // Use the middleware option to add logger
 });
 
-export default appStore; 
+export default appStore;
